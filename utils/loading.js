b569b8cbc75e08ebc91a7945b6b7d4bc9ff700ef
CarlosOjeda809 <ojedatejerinacarlos@gmail.com>
2025-05-20 16:38:49 +0200

    eliminacion darkmode + cambios login

--- utils/loading.js
+++ utils/loading.js
@@ -1,7 +1,7 @@
 export async function loading() {
-      this.loaderElement = document.createElement('div')
-      this.loaderElement.innerHTML = `<div class="fixed inset-0 flex items-center justify-center bg-white z-50" id="loading">
+    this.loaderElement = document.createElement('div')
+    this.loaderElement.innerHTML = `<div class="fixed inset-0 flex items-center justify-center bg-white z-50" id="loading">
           <img src="/img/pokeball.png" class="animate-spin w-20 h-20" alt="Loading..." /></div>`
-      this.loaderElement.style.display = 'none'
+    this.loaderElement.style.display = 'none'
     document.body.appendChild(this.loaderElement)
 }
